fe85ed49b7336d2ae0a740609b7f8f2e811ba06f
ImJunHong <tensai3355@gmail.com>
2021-08-23 01:14:22 +0900

    fix: server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen('8080', function () {
+app.listen(PORT, () => {
   console.log('listening on 8080');
 });
 
 app.use('/', express.static(__dirname + '/'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
