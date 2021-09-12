a74780cb9b5ab8710db88aa497fe9953d9064a01
ImJunHong <tensai3355@gmail.com>
2021-09-12 16:27:39 +0900

    rename: models폴더 만들고 User와 Log 옮김

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 require('dotenv').config({ path: 'variables.env' });
 const app = express();
 const PORT = process.env.PORT || 5000;
-const User = require('./User');
-const Log = require('./Log');
+const User = require('./models/User');
+const Log = require('./models/Log');
 
 app.use(cors());
 app.use(express.json());
