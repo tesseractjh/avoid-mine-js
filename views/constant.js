6c1240e5acb849de2983dc9f4b5312645424c844
ImJunHong <tensai3355@gmail.com>
2021-10-01 14:19:51 +0900

    Update PATCH_DATE

--- views/constant.js
+++ views/constant.js
@@ -1,6 +1,6 @@
 // Constant Values
 const VERSION = 100;
-const PATCH_DATE = '마지막 업데이트: 09/29 오전 12시'
+const PATCH_DATE = '마지막 업데이트: 10/01 오후 2시'
 
 const BACKGROUND_COLOR     = 'rgb( 90,  96, 116)';
 const BLACK                = 'rgb(  0,   0,   0)';
