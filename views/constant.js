4659e0375356210014af0b26f6a45d149050c81e
ImJunHong <tensai3355@gmail.com>
2021-10-29 01:54:39 +0900

    Update BLOCK_MAPS

--- views/constant.js
+++ views/constant.js
@@ -2756,7 +2756,7 @@ const SURVIVAL_DELAY = 10000;
 const SURVIVAL_INIT_RATIO = 0.92;
 const SURVIVAL_FINAL_RATIO = 0.97;
 
-const BLOCK_MAPS = [9, 10, 14, 16];
+const BLOCK_MAPS = [9, 10, 14, 16, 17];
 
 [
   colorMatch, itemMatch, shapeMatch, textHintMatch, RAINBOW, TEXT_HINT,
